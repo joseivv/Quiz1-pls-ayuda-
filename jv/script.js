bf87e46bd2b156660e62baf26c72ac0a966c8a80
celia vera uzcategui <joseivillegasvera@gmail.com>
2020-02-07 21:37:56 -0500

    termine estooo

---
+++ jv/script.js
@@ -0,0 +1,17 @@
+var nombre, correo, asunto, texto;
+
+function mandar(){
+
+nombre = document.getElementById("nombre").value;
+correo = document.getElementById("nombre").value;
+asunto = document.getElementById("nombre").value;
+texto = document.getElementById("nombre").value;
+
+
+    alert("se mando su info bb");
+    console.log("Su nombre es ", this.nombre);
+    console.log("Su correo es ", this.correo);
+    console.log("Su asuto fue ", this.asunto);
+    console.log("Su texto fue ", this.value);
+
+}
